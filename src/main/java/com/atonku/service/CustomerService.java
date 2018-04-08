41cb43ee3af94f12da102fb848bb370bb113a63c
1 <489536564@qq.com>
2018-04-08 10:33:54 +0800

    添加客户service方法

--- src/main/java/com/atonku/service/CustomerService.java
+++ src/main/java/com/atonku/service/CustomerService.java
@@ -1,5 +1,10 @@
 package com.atonku.service;
 
+import com.atonku.entity.Customer;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * 标准MVC 架构中是没有服务层的, 我们将该层作为衔接控制层和数据库之间的桥梁
  * 可以使用接口和实现类的方式来表达, 应根据具体情况来使用, 并非必须使用接口
@@ -12,7 +17,59 @@ package com.atonku.service;
  * @Date: 2018/3/30 16:18
  * @Author: guyatong
  * @Modified by:
+ *
  **/
 public class CustomerService {
+
+    /**
+     * 获取客户列表
+     * @param keyword
+     * @return
+     */
+    public List<Customer> getCustomerList(String keyword){
+        //TODO
+        return null;
+    }
+
+    /**
+     * 获取客户详情
+     * @param id
+     * @return
+     */
+    public Customer getCustomer(long id){
+        //TODO
+        return null;
+    }
+
+    /**
+     * 创建用户
+     * @param fieldMap
+     * @return
+     */
+    public boolean creatCustomer(Map<String, Object> fieldMap){
+        // TODO
+        return false;
+    }
+
+    /**
+     * 更新客户
+     * @param id
+     * @param fieldMap
+     * @return
+     */
+    public boolean updateCusomer(long id, Map<String, Object> fieldMap){
+        //TODO
+        return false;
+    }
+
+    /**
+     * 删除客户
+     * @param id
+     * @return
+     */
+    public boolean deleteCustomer(long id){
+        //TODO
+        return false;
+    }
 
 }
